46c4a749baf194f1f286ba682230e5a351c9adf5
laurencepop <lpemailx@gmail.com>
2023-05-17 03:58:49 +0300

    ok

--- app/test/api/route.ts
+++ app/test/api/route.ts
@@ -1,21 +1,60 @@
 // import Logger from "@/utils/logger"
+import sampleData from "@/home/items.json"
 // import type { NextApiRequest, NextApiResponse } from "next"
 import { NextResponse, type NextRequest } from "next/server"
-
+import { v4 as uuidv4 } from "uuid"
 
 //? https://nextjs.org/docs/app/building-your-application/routing/middleware
 
-interface YYY {
-    message: string | undefined
-}
+/* ---------------------------------- POST ---------------------------------- */
+
+// interface YYY {
+//     message: string | undefined
+// }
 
 // export async function POST(req: NextApiRequest, res: NextApiResponse<YYY>) {
 // export async function POST(req: NextRequest, res: NextResponse<YYY>) {
 export async function POST(req: NextRequest) {
-    const body: YYY = await req.json()
+    // const body: YYY = await req.json()
+    const { name, description, path } = await req.json()
 
-    return NextResponse.json({ body })
-    // return NextResponse.json({ message: done })
+    const newItem = {
+        id: uuidv4(),
+        name,
+        description,
+        path,
+    }
+    // items.push(newItem) -> save to db
+
+    return NextResponse.json({ newItem })
+    // return NextResponse.json({ message: name, description, path })
     // return new Response(JSON.stringify({ message: body }), { status: 200 })
+    // res.status(200).json({ message: `Req method: ${req.method}` })
+}
+
+/* --------------------------------- SEARCH --------------------------------- */
+
+//~ Request Url to this route: ...3000/test/api?query=abc&other=xxx
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url)
+    const param1 = searchParams.get("query") || ""
+    console.log(`Res: ${param1}`) // abc
+    const param2 = searchParams.get("other")
+    console.log(`Res: ${param2}`) // xxx
+
+    // do filtering based on the request params
+    const filteredItem = sampleData.filter((item) => {
+        return item.name.toLowerCase().includes(param1.toLowerCase())
+    })
+
+    // try {
+    //     res.status(200).json({ name: "Mark" })
+    // } catch (err) {
+    //     res.status(400).json(await Logger({ err }))
+    // }
+
+    return NextResponse.json(filteredItem)
+    // return NextResponse.json({ xxxx })
+    // return new Response(JSON.stringify({ message: param }), { status: 200 })
     // res.status(200).json({ message: `Req method: ${req.method}` })
 }
